07de9556a90e4df50e1c9d5079b43a6e5ffb58d5
GroleDev <73291571+GroleDev@users.noreply.github.com>
2025-04-15 19:01:05 +0200

    Fix nation tag when no nation and remove vowels

--- src/main/java/net/laboulangerie/landschat/expansion/LandsChatExpansion.java
+++ src/main/java/net/laboulangerie/landschat/expansion/LandsChatExpansion.java
@@ -3,7 +3,8 @@ package net.laboulangerie.landschat.expansion;
 import org.bukkit.entity.Player;
 
 import me.angeschossen.lands.api.LandsIntegration;
-
+import me.angeschossen.lands.api.land.Land;
+import me.angeschossen.lands.api.nation.Nation;
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 
 import net.laboulangerie.landschat.LandsChat;
@@ -38,11 +39,20 @@ public class LandsChatExpansion extends PlaceholderExpansion {
     @Override
     public String onPlaceholderRequest(Player player, String params) {
         if(params.equals("landtag")) {
-            if(landsAPI.getLandPlayer(player.getUniqueId()).getLands().stream().findFirst().orElse(null) == null) return "";
-            else return "<white>[<aqua>" + landsAPI.getLandPlayer(player.getUniqueId()).getLands().stream().findFirst().orElse(null).getName().substring(0, 4).toUpperCase() + "<white>] ";
+            Land land = landsAPI.getLandPlayer(player.getUniqueId()).getLands().stream().findFirst().orElse(null);
+
+            if(land != null){
+                return "<white>[<aqua>" + land.getName().toUpperCase().replaceAll("[AÂÄEÊËIÎÏOÔÖUÛÜY]", "").substring(0, 4) + "<white>] ";
+            } else {
+                return "";
+            }
         } else if(params.equals("nationtag")) {
-            if(landsAPI.getLandPlayer(player.getUniqueId()).getLands().stream().findFirst().orElse(null).getNation() == null) return "";
-            else return "<white>[<gold>" + landsAPI.getLandPlayer(player.getUniqueId()).getLands().stream().findFirst().orElse(null).getNation().getName().substring(0, 4).toUpperCase() + "<white>] ";
+            try{
+                Nation nation = landsAPI.getLandPlayer(player.getUniqueId()).getLands().stream().findFirst().orElse(null).getNation();
+                return "<white>[<gold>" + nation.getName().toUpperCase().replaceAll("[AÂÄEÊËIÎÏOÔÖUÛÜY]", "").substring(0, 4) + "<white>] ";
+            } catch (Exception e) {
+                return "";
+            }
         } else if(params.equals("nationmembers")) return ((Integer)landsAPI.getLandPlayer(player.getUniqueId()).getLands().stream().findFirst().orElse(null).getNation().getMembersAmount()).toString();
 
         return null;
